d748ee464ab1a275c7ec056e6ea24b1ec117ff4c
xiaoyueyue <1656800216@qq.com>
2017-10-26 14:59:52 +0800

    arguments

--- yue.js
+++ yue.js
@@ -10,11 +10,7 @@
 
 //测试地址
 var URL = "http://11.11.136.102:8080/SHXSKH";
-var PICURL = "http://11.11.136.102:8080";
 
-var offset = "1"; //从第几条开始   
-var limit = "15"; //每页显示行数
-var pageNo = "1"; //页数
 
 //判断数据类型
 function typeOf(value) {
@@ -55,23 +51,6 @@ function typeOf(value) {
             return 'unknow';
     }
 }
-function fomate(obj) {
-      if (typeof obj == 'object' && obj.constructor == Array) {
-        alert('数组')
-      }
-      if (typeof obj == 'string' && obj.constructor == String) {
-        alert('string')
-      } else if (typeof obj == "object" && obj.constructor == Object) {
-        alert('对象')
-      }
-      if (typeof obj == 'number' && obj.constructor == Number) {
-        alert('数值类型')
-      } else if (typeof obj == "object" && obj.constructor == Date) {
-        alert('时间类型')
-      } else if (typeof obj == "function" && obj.constructor == Function) {
-        alert('函数类型')
-      }
-    }
 
 function ajaxPostQuery(url, paramJsonStr, func, dataType) {
     var dataType = dataType || "json";
@@ -194,9 +173,9 @@ Date.prototype.formate = function (format) {
 
 
 //重新加载页面,在ajax回调函数执行后调用
-function reload() {
-    $("#listdetail").html("");
-    getOrderCancel();
+function reload(id,callback) {
+    $("#+'id'").html("");
+   
 }
 
 function switchChoose(id) {
@@ -348,195 +327,3 @@ function fomate20(x) {
     return s;
 }
 
-/**
- * ydui.util
- */
-!function (window) {
-    "use strict";
-
-    var util = window.YDUI.util = window.YDUI.util || {},
-        doc = window.document;
-
-    /**
-     * 日期格式化
-     * @param format 日期格式 {%d天}{%h时}{%m分}{%s秒}{%f毫秒}
-     * @param time 单位 毫秒
-     * @returns {string}
-     */
-    util.timestampTotime = function (format, time) {
-        var t = {},
-            floor = Math.floor;
-
-        t.f = time % 1000;
-        time = floor(time / 1000);
-        t.s = time % 60;
-        time = floor(time / 60);
-        t.m = time % 60;
-        time = floor(time / 60);
-        t.h = time % 24;
-        t.d = floor(time / 24);
-
-        var ment = function (a) {
-            if (a <= 0) {
-                return '';
-            }
-            return '$1' + (a < 10 ? '0' + a : a) + '$2';
-        };
-
-        format = format.replace(/\{([^{]*?)%d(.*?)\}/g, ment(t.d));
-        format = format.replace(/\{([^{]*?)%h(.*?)\}/g, ment(t.h));
-        format = format.replace(/\{([^{]*?)%m(.*?)\}/g, ment(t.m));
-        format = format.replace(/\{([^{]*?)%s(.*?)\}/g, ment(t.s));
-        format = format.replace(/\{([^{]*?)%f(.*?)\}/g, ment(t.f));
-
-        return format;
-    };
-
-    /**
-     * js倒计时
-     * @param format 时间格式 {%d}天{%h}时{%m}分{%s}秒{%f}毫秒
-     * @param time 结束时间时间戳 毫秒
-     * @param speed 速度
-     * @param callback ret 倒计时结束回调函数 ret 时间字符 ；ret == '' 则倒计时结束
-     * DEMO: YDUI.util.countdown('{%d天}{%h时}{%m分}{%s秒}{%f毫秒}', Date.parse(new Date()) + 60000, 1000, function(ret){ console.log(ret); });
-     */
-    util.countdown = function (format, time, speed, callback) {
-        var that = this;
-        var timer = setInterval(function () {
-            var l_time = time - new Date().getTime();
-            if (l_time > 0) {
-                callback(that.timestampTotime(format, l_time));
-            } else {
-                clearInterval(timer);
-                typeof callback == 'function' && callback('');
-            }
-        }, speed);
-    };
-
-    /**
-     * js 加减乘除
-     * @param arg1 数值1
-     * @param op 操作符string 【+ - * /】
-     * @param arg2 数值2
-     * @returns {Object} arg1 与 arg2运算的精确结果
-     */
-    util.calc = function (arg1, op, arg2) {
-        var ra = 1, rb = 1, m;
-
-        try {
-            ra = arg1.toString().split('.')[1].length;
-        } catch (e) {
-        }
-        try {
-            rb = arg2.toString().split('.')[1].length;
-        } catch (e) {
-        }
-        m = Math.pow(10, Math.max(ra, rb));
-
-        switch (op) {
-            case '+':
-            case '-':
-                arg1 = Math.round(arg1 * m);
-                arg2 = Math.round(arg2 * m);
-                break;
-            case '*':
-                ra = Math.pow(10, ra);
-                rb = Math.pow(10, rb);
-                m = ra * rb;
-                arg1 = Math.round(arg1 * ra);
-                arg2 = Math.round(arg2 * rb);
-                break;
-            case '/':
-                arg1 = Math.round(arg1 * m);
-                arg2 = Math.round(arg2 * m);
-                m = 1;
-                break;
-        }
-        try {
-            var result = eval('(' + '(' + arg1 + ')' + op + '(' + arg2 + ')' + ')/' + m);
-        } catch (e) {
-        }
-        return result;
-    };
-
-    /**
-     * 读取图片文件 并返回图片的DataUrl
-     * @param obj
-     * @param callback
-     */
-    util.getImgBase64 = function (obj, callback) {
-        var that = this, dataimg = '', file = obj.files[0];
-        if (!file)return;
-        if (!/image\/\w+/.test(file.type)) {
-            that.tipMes('请上传图片文件', 'error');
-            return;
-        }
-        var reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function () {
-            dataimg = this.result;
-            typeof callback === 'function' && callback(dataimg);
-        };
-    };
-
-    /**
-     * 获取地址栏参数
-     * @param name
-     * @returns {*}
-     */
-    util.getQueryString = function (name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
-            r = window.location.search.substr(1).match(reg),
-            qs = '';
-        if (r != null)qs = decodeURIComponent(r[2]);
-        return qs;
-    };
-
-    /**
-     * Cookie
-     * @type {{get, set}}
-     */
-    util.cookie = function () {
-        return {
-            /**
-             * 获取 Cookie
-             * @param  {String} name
-             * @return {String}
-             */
-            get: function (name) {
-                var m = doc.cookie.match('(?:^|;)\\s*' + name + '=([^;]*)');
-                return (m && m[1]) ? decodeURIComponent(m[1]) : '';
-            },
-            /**
-             * 设置 Cookie
-             * @param {String}  name 名称
-             * @param {String}  val 值
-             * @param {Number} expires 单位（秒）
-             * @param {String}  domain 域
-             * @param {String}  path 所在的目录
-             * @param {Boolean} secure 跨协议传递
-             */
-            set: function (name, val, expires, domain, path, secure) {
-                var text = String(encodeURIComponent(val)),
-                    date = expires;
-
-                // 从当前时间开始，多少小时后过期
-                if (typeof date === 'number') {
-                    date = new Date();
-                    date.setTime(date.getTime() + expires * 1000);
-                }
-
-                date instanceof Date && (text += '; expires=' + date.toUTCString());
-
-                !!domain && (text += '; domain=' + domain);
-
-                text += '; path=' + (path || '/');
-
-                secure && (text += '; secure');
-
-                doc.cookie = name + '=' + text;
-            }
-        }
-    }();
-
-}(window);
