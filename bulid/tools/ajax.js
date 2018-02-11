db77fbb652afacfef978a991b4c0fbb5aeb0e6e9
xiaoyueyue <1656800216@qq.com>
2018-02-11 14:41:57 +0800

    * 简短的描述干了什么？ What: dom.js方法的添加
    * 我为什么要这么做？
    Why:
    
    * 我是怎么做的？这么做会有什么副作用？
    How:

--- bulid/tools/ajax.js
+++ bulid/tools/ajax.js
@@ -1,51 +1,45 @@
 // 获取地址栏request中的参数
 function GetQueryString(name) {
-
-    const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    const r = window
-        .location
-        .search
-        .substr(1)
-        .match(reg);
-    if (r != null)
-        return unescape(r[2]);
-    return null;
+  const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  const r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]);
+  return null;
 }
 function ajaxPostQuery(url, paramJsonStr, func, dataType) {
-    var dataType = dataType || "json";
-    var url = url || queryUrl;
-    var sid = '123'; //用户登录的sessionID
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: paramJsonStr + "&sid=" + sid,
-        headers: {
-            accept: "*/*"
-        },
-        //contentType:"application/x-www-form-urlencoded",
-        dataType: dataType,
-        timeout: 0,
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert("网络暂时不可用");
-        },
-        success: function (data) {
-            var errorData;
-            if (typeof data == "string") {
-                try {
-                    errorData = eval("(" + data + ")");
+  var dataType = dataType || "json";
+  var url = url || queryUrl;
+  var sid = "123"; //用户登录的sessionID
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: paramJsonStr + "&sid=" + sid,
+    headers: {
+      accept: "*/*"
+    },
+    //contentType:"application/x-www-form-urlencoded",
+    dataType: dataType,
+    timeout: 0,
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      alert("网络暂时不可用");
+    },
+    success: function(data) {
+      var errorData;
+      if (typeof data == "string") {
+        try {
+          errorData = eval("(" + data + ")");
 
-                    if (errorData.error == "-1") {
-                        alert(url);
-                        // relogin(url);
-                    } else {
-                        func(data);
-                    }
-                } catch (e) {
-                    func(data);
-                }
-            }
+          if (errorData.error == "-1") {
+            alert(url);
+            // relogin(url);
+          } else {
+            func(data);
+          }
+        } catch (e) {
+          func(data);
         }
-    });
+      }
+    }
+  });
 }
 /**
  *
@@ -54,19 +48,23 @@ function ajaxPostQuery(url, paramJsonStr, func, dataType) {
  * @return {Object}
  */
 function parseQueryString(url) {
-    url = url == null ?
-        window.location.href :
-        url
+  url = url == null ? window.location.href : url;
 
-    var search = url.substring(url.lastIndexOf('?') + 1)
+  var search = url.substring(url.lastIndexOf("?") + 1);
 
-    if (!search) {
+  if (!search) {
+    return {};
+  }
 
-        return {}
-    }
-
-    return
-    JSON.parse('{"' + decodeURIComponent(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+  return;
+  JSON.parse(
+    '{"' +
+      decodeURIComponent(search)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"') +
+      '"}'
+  );
 }
 
 /**
@@ -75,67 +73,61 @@ function parseQueryString(url) {
  * @param  {Object} obj
  * @return {String}
  */
-function
-stringfyQueryString(obj) {
+function stringfyQueryString(obj) {
+  if (!obj) return;
 
-    if (!obj)
-        return
+  ("");
 
+  var pairs = [];
 
-    '';
+  for (var key in obj) {
+    var value = obj[key];
 
-    var pairs = [];
+    if (value instanceof Array) {
+      for (var i = 0; i < value.length; ++i) {
+        pairs.push(
+          encodeURIComponent(key + "[" + i + "]") +
+            "=" +
+            encodeURIComponent(value[i])
+        );
+      }
 
-    for (var key in obj) {
-
-        var value = obj[key];
-
-        if (value instanceof Array) {
-
-            for (var i = 0; i < value.length; ++i) {
-                pairs.push(encodeURIComponent(key + '[' + i + ']') + '=' + encodeURIComponent(value[i]));
-            }
-
-            continue;
-        }
-        pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+      continue;
     }
+    pairs.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]));
+  }
 
-    return
-    pairs.join('&');
+  return pairs.join("&");
 }
 
 //设置cookie
 function setCookie(c_name, value, expiredays) {
-    var exdate = new Date()
-    exdate.setDate(exdate.getDate() + expiredays)
-    document.cookie = c_name + "=" + escape(value) + ((expiredays == null)
-        ? ""
-        : ";expires=" + exdate.toGMTString())
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + expiredays);
+  document.cookie =
+    c_name +
+    "=" +
+    escape(value) +
+    (expiredays == null ? "" : ";expires=" + exdate.toGMTString());
 }
 
-setCookie('zhangsan', 18, 1)
+setCookie("zhangsan", 18, 1);
 
 //获取
 function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-        c_start = document
-            .cookie
-            .indexOf(c_name + "=")
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1
-            c_end = document
-                .cookie
-                .indexOf(";", c_start)
-            if (c_end == -1)
-                c_end = document.cookie.length
-            return unescape(document.cookie.substring(c_start, c_end))
-        }
+  if (document.cookie.length > 0) {
+    c_start = document.cookie.indexOf(c_name + "=");
+    if (c_start != -1) {
+      c_start = c_start + c_name.length + 1;
+      c_end = document.cookie.indexOf(";", c_start);
+      if (c_end == -1) c_end = document.cookie.length;
+      return unescape(document.cookie.substring(c_start, c_end));
     }
-    return ""
+  }
+  return "";
 }
 
 //删除
 function clearCookie(name) {
-    setCookie(name, "", -1);
+  setCookie(name, "", -1);
 }
--- bulid/tools/dom.js
+++ bulid/tools/dom.js
@@ -1,3 +1,44 @@
+var dom = {
+  on(element, eventType, selector, fn) {
+    element.addEventListener(eventType, e => {
+      let el = e.target;
+      while (!el.matches(selector)) {
+        if (element === el) {
+          el = null;
+          break;
+        }
+        el = el.parentNode;
+      }
+      el && fn.call(el, e, el);
+    });
+    return element;
+  },
+  index(element) {
+    const siblings = element.parentNode.children;
+    for (let index = 0; index < siblings.length; index++) {
+      if (siblings[index] === element) {
+        return index;
+      }
+    }
+    return -1;
+  },
+  //排他思想方式添加class
+  uniqueClass(element, className) {
+    dom.every(element.parentNode.children, el => {
+      el.classList.remove(className); // 排他
+    });
+    element.classList.add(className);
+    return element;
+  },
+
+  every(nodeList, fn) {
+    for (var i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i], i);
+    }
+    return nodeList;
+  }
+};
+
 function $(selector, el) {
   if (!el) {
     el = document;
