5e49add2ea5b182c334cbda09d85a92f573c2354
xiaoyueyue <1656800216@qq.com>
2018-04-17 18:32:46 +0800

    add event+checkbox * subject
    * Body
     * what
     * why
     * how
    * Footer

--- tools/device.js
+++ tools/device.js
@@ -1,3 +1,9 @@
+var u = navigator.userAgent, app = navigator.appVersion;
+var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+alert('是否是Android：'+isAndroid);
+alert('是否是iOS：'+isiOS);
+
 function getBrowser() {
   var t = navigator.userAgent.toLowerCase();
   if (t.indexOf("android") >= 0)
