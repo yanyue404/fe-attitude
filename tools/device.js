847053a8fbd20b2ac5b16c1c812cd3bf62a9d8f9
xiaoyueyue <xiaoyueyue165@gmail.com>
2018-05-01 00:00:01 +0800

    Update device.js

--- tools/device.js
+++ tools/device.js
@@ -62,6 +62,38 @@ if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|wOSB
     isMobile = false;
 }
 
+var Browser = (function() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    return {
+        get: function() {
+            if (userAgent.indexOf("msie") > 0) {
+                return "msie"
+            }
+            if (userAgent.indexOf("firefox") > 0) {
+                return "firefox"
+            }
+            if (userAgent.indexOf("chrome") > 0) {
+                return "chrome"
+            }
+        }
+    }
+}
+)();
+var OS = (function() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    return {
+        get: function() {
+            if (userAgent.indexOf("mac os") > 0) {
+                return "mac"
+            }
+            if (userAgent.indexOf("win") > 0) {
+                return "win"
+            }
+        }
+    }
+}
+)();
+
 //用js根据客户端输出对应样式
 function loadCSS(url) {
     if (!(navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|wOSBrowser|BrowserNG|WebOS)/i))) {
