8ba4e18a2c16efb95ff71ab9753a3da6f9d94eed
xiaoyueyue <1656800216@qq.com>
2018-05-04 15:32:15 +0800

    add mvvm * subject
    * Body
     * what
     * why
     * how
    * Footer

---
+++ mvvm/README.md
@@ -0,0 +1,2 @@
+## 剖析Vue实现原理 - 如何实现双向绑定mvvm
+- https://github.com/DMQ/mvvm
---
+++ 算法类/递归/demo2-斐波那契数列.js
@@ -0,0 +1,4 @@
+function fibonacci(n) {
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(5))
