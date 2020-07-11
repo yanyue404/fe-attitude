af09bc164c232477337dabbe6f9efb5e1f245a6a
yanyue404 <xiaoyueyue165@gmail.com>
2020-07-11 13:49:32 +0800

    feat: Storage & promise

--- utils/modules/storage.js
+++ utils/modules/storage.js
@@ -1,11 +1,9 @@
 class StorageFn {
   constructor() {
     this.ls = window.localStorage;
-    this.ss = window.sessionStorage;
   }
 
   /*-----------------cookie---------------------*/
-  /*设置cookie*/
   setCookie(name, value, day) {
     var setting = arguments[0];
     if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object') {
@@ -21,7 +19,6 @@ class StorageFn {
     }
   }
 
-  /*获取cookie*/
   getCookie(name) {
     var arr = document.cookie.split('; ');
     for (var i = 0; i < arr.length; i++) {
@@ -33,66 +30,61 @@ class StorageFn {
     return '';
   }
 
-  /*删除cookie*/
   removeCookie(name) {
     this.setCookie(name, 1, -1);
   }
 
   /*-----------------localStorage---------------------*/
-  /*设置localStorage*/
-  setLocal(key, val) {
-    var setting = arguments[0];
-    if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object') {
-      for (var i in setting) {
-        this.ls.setItem(i, JSON.stringify(setting[i]));
-      }
-    } else {
-      this.ls.setItem(key, JSON.stringify(val));
+  setItem(key, val, expires) {
+    // 设置过期时间 https://github.com/Advanced-Frontend/Daily-Interview-Question/issues/171
+    if (typeof expires !== 'undefined') {
+      var expiresDate = new Date(expires).valueOf();
+      this.ls.setItem(key + '_expires', expiresDate);
     }
+    this.ls.setItem(key, val);
   }
 
-  /*获取localStorage*/
-  getLocal(key) {
-    if (key) return JSON.parse(this.ls.getItem(key));
+  getItem(key) {
+    var expires = this.ls.getItem(key + '_expires');
+    if (expires && new Date() > new Date(Number(expires))) {
+      this.ls.removeItem(key);
+      this.ls.removeItem(key + '_expires');
+    }
+    if (key) return this.ls.getItem(key);
     return null;
   }
 
-  /*移除localStorage*/
-  removeLocal(key) {
+  removeItem(key) {
     this.ls.removeItem(key);
   }
 
   /*移除所有localStorage*/
-  clearLocal() {
+  clear() {
     this.ls.clear();
   }
-
-  /*-----------------sessionStorage---------------------*/
-  /*设置sessionStorage*/
-  setSession(key, val) {
-    var setting = arguments[0];
-    if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object') {
-      for (var i in setting) {
-        this.ss.setItem(i, JSON.stringify(setting[i]));
-      }
-    } else {
-      this.ss.setItem(key, JSON.stringify(val));
-    }
-  }
-
-  /*获取sessionStorage*/
-  getSession(key) {
-    if (key) return this.ss.getItem(key);
-    return null;
-  }
-
-  /*移除sessionStorage*/
-  removeSession(key) {
-    this.ss.removeItem(key);
-  }
-
-  /*移除所有sessionStorage*/
-  clearSession() {
-    this.ss.clear();
-  }
 }
+
+/* const Storage = new StorageFn();
+Storage.setItem('key', 'value', new Date(Date.now() + 10000)); // 10 秒钟后过期
+Storage.getItem('key'); */
+
+/* (function() {
+  var getItem = localStorage.getItem.bind(localStorage);
+  var setItem = localStorage.setItem.bind(localStorage);
+  var removeItem = localStorage.removeItem.bind(localStorage);
+  localStorage.getItem = function(keyName) {
+    var expires = getItem(keyName + '_expires');
+    if (expires && new Date() > new Date(Number(expires))) {
+      removeItem(keyName);
+      removeItem(keyName + '_expires');
+    }
+    return getItem(keyName);
+  };
+  localStorage.setItem = function(keyName, keyValue, expires) {
+    if (typeof expires !== 'undefined') {
+      var expiresDate = new Date(expires).valueOf();
+      setItem(keyName + '_expires', expiresDate);
+    }
+    return setItem(keyName, keyValue);
+  };
+})(); */
