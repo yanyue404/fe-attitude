4913022fae9c28da48659a1fb9fcecda3e3a19a3
RainBow <xiaoyueyue165@gmail.com>
2021-07-31 00:04:21 +0800

    chore: vue api

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  printWidth: 80,
-  singleQuote: true,
-  trailingComma: 'all',
+  printWidth: 120, // 超过最大值换行
+  semi: true, //  要分号分号
+  singleQuote: false, //  要单引号
+  htmlWhitespaceSensitivity: "ignore" // HTML 空白敏感度 (闭合标签换行)
 };
