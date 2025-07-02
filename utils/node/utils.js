ddccc472fcb17058af220cd7aebe8c812a9efed5
yanyue404 <xiaoyueyue165@gmail.com>
2025-07-02 15:58:45 +0800

    chore: 安全的文件名

--- utils/node/utils.js
+++ utils/node/utils.js
@@ -198,6 +198,28 @@ async function getStagedFiles() {
   }
 }
 
+/** 使用 Unicode 字符替换文件名中的特殊字符 */
+const replaceFileName = (fileName) => {
+  // https://docs.microsoft.com/zh-cn/windows/desktop/FileIO/naming-a-file#naming_conventions
+  const replaceMap = new Map([
+    ['<', '\uFF1C'], // Fullwidth Less-Than Sign
+    ['>', '\uFF1E'], // Fullwidth Greater-Than Sign
+    [':', '\uFF1A'], // Fullwidth Colon
+    ['/', '\uFF0F'], // Fullwidth Solidus
+    ['\\', '\uFF3C'], // Fullwidth Reverse Solidus
+    ['|', '\uFF5C'], // Fullwidth Vertical Line
+    ['?', '\uFF1F'], // Fullwidth Question Mark
+    ['*', '\uFF0A'], // Fullwidth Asterisk
+    ['"', '\uFF02'] // Fullwidth Quotation Mark
+  ])
+
+  const pattern = [...replaceMap.keys()].map((key) => '\\' + key).join('|')
+
+  const regex = new RegExp(pattern, 'g')
+
+  return fileName.replace(regex, (match) => replaceMap.get(match))
+}
+
 // 示例用法
 // const getStagedFiles = require("./getStagedFiles");
 // (async () => {
@@ -218,5 +240,6 @@ module.exports = {
   isGitRepository,
   getGitRoot,
   writeExecutableFile,
-  getStagedFiles
+  getStagedFiles,
+  replaceFileName
 }
